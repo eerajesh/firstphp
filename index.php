e221ae9eb0ff3812fb669bc3548a2bc8d9e25325
eerajesh <eerajesh@gmail.com>
2019-01-31 14:07:35 -0500

    Update index.php
    1.3

--- index.php
+++ index.php
@@ -3,7 +3,8 @@
   <title>PHP Test</title>
  </head>
  <body>
- <?php echo '<p>Hello World Rajesh on going school</p>'; ?> 
+ <?php echo '<p>Hello World Rajesh on going school</p>'; ?>
+  <?php echo '<p>Hello World devashish and mokshaja are going to school</p>'; ?> 
   <?php echo '<p>Hello team where you are going. Rajesh on going school</p>'; ?>
  </body>
 </html>
